8b87dd691e39693ea639994d14d81e1ccbbb2d5a
S_HYEON <zxl3651@naver.com>
2024-01-23 14:47:27 +0900

    🎉 [Init] 프로젝트 초기 생성

---
+++ VoiceMemoApp/VoiceMemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceMemoApp
+//
+//  Created by 이성현 on 2024/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                
+            Text("Hello, world!")
+                .strikethrough()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceMemoApp/VoiceMemoApp/VoiceMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoAppApp.swift
+//  VoiceMemoApp
+//
+//  Created by 이성현 on 2024/01/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
